ec4d1bf5acf8e0f5f682a392a751a2b9fbf73d49
rab63 <rab63@95ac2072-b062-11df-b05e-a70d922a01dc>
2016-06-11 13:03:56 +0000

    Blunder

--- OpenSpecimen/trunk/v3.1/openspecimen_v3_1/first_run_open_specimen.sh
+++ OpenSpecimen/trunk/v3.1/openspecimen_v3_1/first_run_open_specimen.sh
@@ -11,9 +11,6 @@ sleep 60
 echo "Now stopping OpenSpecimen..."
 $OPEN_SPECIMEN_DIR/stop_open_specimen.sh
 
-echo "Now fixing the incorrect Schema.xml"
-cp $OPEN_SPECIMEN_DIR/fixed_db_2_0_schema.xml $OPEN_SPECIMEN_DIR/app/webapps/openspecimen/WEB-INF/classes/db/2.0/schema.xml
-
 setfacl -R -m u:wwwrun:rwx $OPEN_SPECIMEN_DIR/app/logs/
 setfacl -R -m u:wwwrun:rwx $OPEN_SPECIMEN_DIR/app/work/
 setfacl -R -m u:wwwrun:rx $OPEN_SPECIMEN_DIR/app/webapps/
