d4b25127d37fb9286fb7609f964957e2460ae295
rab63 <rab63@95ac2072-b062-11df-b05e-a70d922a01dc>
2016-06-20 12:54:17 +0000

    Add v3.0

---
+++ OpenSpecimen/trunk/v3.0/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+OPEN_SPECIMEN_DIR=/local/openSpecimen_v3_0
+
+cp -R deploy $OPEN_SPECIMEN_DIR
+
+setfacl -m u:wwwrun:rx $OPEN_SPECIMEN_DIR/run_open_specimen.sh
+setfacl -m u:wwwrun:rx $OPEN_SPECIMEN_DIR/stop_open_specimen.sh
+setfacl -R -m u:wwwrun:rwx $OPEN_SPECIMEN_DIR/app/logs/
+setfacl -R -m u:wwwrun:rwx $OPEN_SPECIMEN_DIR/app/work/
+setfacl -R -m u:wwwrun:rwx $OPEN_SPECIMEN_DIR/app/webapps/
+setfacl -R -m u:wwwrun:rwx $OPEN_SPECIMEN_DIR/app/plugins/
+setfacl -R -m u:wwwrun:rwx $OPEN_SPECIMEN_DIR/app/osdata
+
+grep -rl {{apphost-port}} $OPEN_SPECIMEN_DIR/app/ | xargs sed -i s/{{apphost-port}}/8280/g
+
+chmod 774 $OPEN_SPECIMEN_DIR/app/bin/catalina.sh
+chmod 774 $OPEN_SPECIMEN_DIR/app/bin/shutdown.sh
+setfacl -m u:wwwrun:rx $OPEN_SPECIMEN_DIR/app/bin/catalina.sh
+
