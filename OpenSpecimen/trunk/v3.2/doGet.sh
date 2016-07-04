ac98bdec99c2c05bc3cd5bbabb46ccf143ecaea9
rab63 <rab63@95ac2072-b062-11df-b05e-a70d922a01dc>
2016-07-04 11:04:45 +0000

    Remove build directory for le_plugin zip

--- OpenSpecimen/trunk/v3.2/doGet.sh
+++ OpenSpecimen/trunk/v3.2/doGet.sh
@@ -2,6 +2,9 @@
 
 OS_VERSION='3.2'
 
-wget  --output-document=build/openspecimen-$OS_VERSION.zip https://github.com/krishagni/openspecimen/releases/tag/v$OS_VERSION
-svn export https://github.com/krishagni/os-extensions.git/trunk/le build/le_plugin
-zip -rm build/le_plugin.zip build/le_plugin
+cd build
+
+wget  --output-document=openspecimen-$OS_VERSION.zip https://github.com/krishagni/openspecimen/releases/tag/v$OS_VERSION
+svn export https://github.com/krishagni/os-extensions.git/trunk/le le_plugin
+
+zip -rm le_plugin.zip le_plugin
