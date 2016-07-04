ea1a2d56f4105258b8cee5b933a9d4289d1f859b
rab63 <rab63@95ac2072-b062-11df-b05e-a70d922a01dc>
2016-07-04 11:04:45 +0000

    Remove build directory for le_plugin zip

--- v3.2/doBuild.sh
+++ v3.2/doBuild.sh
@@ -3,15 +3,32 @@
 OS_VERSION='3.2'
 
 function buildOpenSpecimen {
+	cd build
+
+	unzip le_plugin.zip
+
+	cd /openspecimen-$OS_VERSION/www
+	npm install
+	bower install
+
+	cd ..
+	gradle build
+
+	cd ../..
+	cp build/openspecimen-$OS_VERSION/build/libs/openspecimen.war deploy/builds/openspecimen-$1.war
+
+	rm -fR build/openspecimen-$OS_VERSION
+}
+
+function buildOpenSpecimen {
     rm deploy/builds/openspecimen-$1.war
 
 	cd build
 	unzip openspecimen-$OS_VERSION.zip
 
-	cd ..
-	cp build/build.properties.$1 build/openspecimen-$OS_VERSION/build.properties
+	cp build.properties.$1 build/openspecimen-$OS_VERSION/build.properties
 
-	cd build/openspecimen-$OS_VERSION/www
+	cd openspecimen-$OS_VERSION/www
 	npm install
 	bower install
 
