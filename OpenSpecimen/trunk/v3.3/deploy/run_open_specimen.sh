d8ab93c0b806455beef1965141514d16c9cfa42a
rab63 <rab63@95ac2072-b062-11df-b05e-a70d922a01dc>
2016-12-10 11:27:57 +0000

    In crease memory options

--- OpenSpecimen/trunk/v3.3/deploy/run_open_specimen.sh
+++ OpenSpecimen/trunk/v3.3/deploy/run_open_specimen.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+JAVA_PROCESS_ID=`pgrep java`
+
+if [ "${JAVA_PROCESS_ID:-null}" != null ]; then
+    echo "Cannot upgrade because java process is still running.  Process ID = $JAVA_PROCESS_ID"
+    exit 1
+fi
+
 unset JAVA_TOOL_OPTIONS
 export JAVA_ROOT=/usr/lib64/jvm/java
 export JDK_HOME=/usr/lib64/jvm/java
@@ -10,8 +17,7 @@ JAVA_HOME=/opt/java
 JRE_HOME=/opt/java/jre
 OPEN_SPECIMEN_DIR=/local/openSpecimen/app
 
-export JAVA_TOOL_OPTIONS=-XX:CompressedClassSpaceSize=10m
-export JAVA_OPTS="-Xmx2G -XX:-UseCompressedClassPointers"
+export JAVA_OPTS="-Xmx2G -XX:-UseCompressedClassPointers -XX:CompressedClassSpaceSize=1g"
 
 $OPEN_SPECIMEN_DIR/bin/catalina.sh start >$OPEN_SPECIMEN_DIR/logs/tomcat-start.log  2>$OPEN_SPECIMEN_DIR/logs/tomcat-start.err &
 
